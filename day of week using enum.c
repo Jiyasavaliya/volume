6d82966d5ca4d5542a12f51495b5227ac5c8df8d
jiyasavaliya18 <126229948+jiyasavaliya18@users.noreply.github.com>
2023-03-31 12:29:38 +0530

    Add files via upload

---
+++ day of week using enum.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<string.h>
+	
+	enum week{
+		
+		mon = 10, tue, wed, thur = 50, fir, sat, sun
+	};
+	
+	
+	int main(){
+		
+		printf("%d",fir);
+		
+		return 0;
+	}
+	
+
